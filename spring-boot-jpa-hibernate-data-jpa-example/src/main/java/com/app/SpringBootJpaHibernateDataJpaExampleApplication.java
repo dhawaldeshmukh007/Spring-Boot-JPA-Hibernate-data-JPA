217c72ece5dbc833badd3c48f5ad53c3c8687725
Dhawal <Dhawal@DESKTOP-G05BL35>
2022-05-23 11:25:30 +0530

    Spring Boot JPA Hibernate Integration

---
+++ spring-boot-jpa-hibernate-data-jpa-example/src/main/java/com/app/SpringBootJpaHibernateDataJpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaHibernateDataJpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaHibernateDataJpaExampleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-jpa-hibernate-data-jpa-example/src/main/java/com/app/entity/Employee.java
@@ -0,0 +1,33 @@
+package com.app.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employee1")
+@Setter
+@Getter
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	// @GenericGenerator(name = "native", strategy = "native")
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "password")
+	private String password;
+
+}
